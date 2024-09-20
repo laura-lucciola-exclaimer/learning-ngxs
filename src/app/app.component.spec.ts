9aa486ac9e1306f5123ee491de44ea8eef56d368
Laura Lucciola <laura.lucciola@exclaimer.com>
2024-09-20 10:41:56 +0100

    test: AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,34 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {  By } from '@angular/platform-browser';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsModule } from '@ngxs/store';
+import { UserComponent } from './user/user.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        NgxsModule.forRoot([]),
+        NgxsLoggerPluginModule.forRoot(),
+        NgxsReduxDevtoolsPluginModule.forRoot(),
+        AppComponent,
+        UserComponent
+      ]
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('should create the app component', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'learning-ngxs' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('learning-ngxs');
-  });
-
-  it('should render title', () => {
+  it('should render the user component', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, learning-ngxs');
+    const userComponent = fixture.debugElement.query(By.directive(UserComponent));
+    expect(userComponent).toBeTruthy();
   });
 });
